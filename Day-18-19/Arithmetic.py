7e83938a5ebbdff4610a00bcb2b01d4863018b71
niharika.shetty26 <niharika.shetty@successive.tech>
2024-05-16 17:55:44 +0530

    fix:16-May

---
+++ Day-18-19/Arithmetic.py
@@ -0,0 +1,11 @@
+# Addition
+a = 5 + 3
+
+# Subtraction
+b = 7 - 2
+
+# Multiplication
+c = 4 * 6
+
+# Division
+d = 10 / 2
---
+++ Day-18-19/Variables.py
@@ -0,0 +1,10 @@
+
+x = 5
+name = "Niharika"
+pi = 3.14
+is_student = True
+
+print(x)
+print(name)
+print(pi)
+print(is_student)
