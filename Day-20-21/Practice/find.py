edf9b7d80dbf15d58cdc65e3d2b1b2283123ca86
niharika.shetty26 <niharika.shetty@successive.tech>
2024-05-20 19:48:23 +0530

    21-May-Practice-Assignment-completed

---
+++ Day-20-21/Practice/find.py
@@ -0,0 +1,19 @@
+# will return index
+word = 'Niharika'
+print(word.find('rik'))
+
+
+
+#.format(parameters/arg)
+name = "Niharika"
+age = 23
+message = "My name is {0} and I am {1} years old. {1} is my favorite number.".format(name, age)
+print(message)
+
+
+val = 'Niharika'
+age= "23"
+print(f"{val} is {age} years old ")
+
+str = '-'.join('What a day')
+print(str)
---
+++ Day-20-21/Practice/stringMethods.py
@@ -0,0 +1,22 @@
+text = 'Niharika'
+
+# upper() function to convert
+print("\nConverted to upper case String:")
+print(text.upper())
+
+# lower() function to convert
+print("\nConverted to lower case String:")
+print(text.lower())
+
+# converts the first character to upper case and rest to lower case
+print("\nConverted to first char cap String:")
+print(text.title())
+
+# swaps the case of all characters in the string upper case character to lowercase and viceversa
+print("\nConverted to all swap char of  String:")
+print(text.swapcase())
+
+# convert the first character of a string to uppercase
+print("\nConverted first char of String to uppercase:")
+print(text.capitalize())
+
