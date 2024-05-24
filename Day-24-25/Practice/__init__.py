80e243d0765e3a12aeab149ee79dbee1b5f0de67
niharika.shetty26 <niharika.shetty@successive.tech>
2024-05-24 18:22:41 +0530

    24-May-Practice-Practice-completed

---
+++ Day-24-25/Practice/__init__.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self,fullname):
+        self.name=fullname;
+        print(self)
+
+s1=Student("Niharika Shetty")
+print(s1)
+print(s1.name)
---
+++ Day-24-25/Practice/class_creation.py
@@ -0,0 +1,4 @@
+class Student:
+    name="Niharika Shetty"
+s1=Student()
+print(s1.name)
