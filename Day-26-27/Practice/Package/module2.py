2c049c6bb40db704042a109176a9e823b3eeb35e
niharika.shetty26 <niharika.shetty@successive.tech>
2024-05-29 16:58:32 +0530

    day26-27

---
+++ Day-26-27/Practice/Package/module2.py
@@ -0,0 +1,3 @@
+
+def function2():
+    return "This is function2 from module2"
---
+++ Day-26-27/Practice/main.py
@@ -0,0 +1,7 @@
+
+import Package
+
+print(Package.function1())
+print(Package.function2())
+print(Package.sum(1,2))
+
