1b0b2b618fa04facca9662910b5e11e1f3f0ee6b
niharika.shetty26 <niharika.shetty@successive.tech>
2024-06-05 15:08:27 +0530

    4-June-Practice+Assignment

--- Day-28-29/Assignment/generator_time_fibbo.py
+++ Day-28-29/Assignment/generator_time_fibbo.py
@@ -2,6 +2,7 @@ import time
 def timing_decorator(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
+        time.sleep(5)
         result = func(*args, **kwargs)
         end_time = time.time()
         print(f"Function {func.__name__} took {end_time - start_time:.4f} seconds")
