7829d72cff900572d04a53aa8db3dd22dad76970
niharika.shetty26 <niharika.shetty@successive.tech>
2024-06-06 14:36:55 +0530

    fix:insert-query

---
+++ create-1.session.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE Student(
+--     FirstName VARCHAR(50),
+--     LastName VARCHAR(50),
+--     RollNo INT NOT NULL,
+--     GRADE VARCHAR(10),
+--     DOB DATE,
+--     PRIMARY KEY (RollNo)
+-- );
+-- INSERT INTO Student (FirstName, LastName, RollNo, GRADE, DOB)
+-- VALUES ('Mishti', 'Sethi', 1, 'A', '2015-12-23');
+-- INSERT INTO Student (FirstName, LastName, RollNo, GRADE, DOB)
+-- VALUES ('Ravi', 'Kumar', 2, 'B', '2016-03-15'),
+--     ('Anya', 'Sharma', 3, 'A', '2015-07-19'),
+--     ('Rahul', 'Verma', 4, 'C', '2016-11-25'),
+--     ('Sneha', 'Patel', 5, 'B', '2015-09-30');
+SELECT *
+FROM Student
