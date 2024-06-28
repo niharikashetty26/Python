7f65f099d340e925b2ad847ad4edc4acee18e18d
niharika.shetty26 <niharika.shetty@successive.tech>
2024-06-28 14:41:41 +0530

    fix:

---
+++ Day-49/Day-49.py
@@ -0,0 +1,5 @@
+#
+# Scenario: The bookstore wants to offer discounts on books published before a certain year.
+#
+# Assignment: Write a SQL query using a subquery or a derived table to identify books published
+# before the year 2000 and apply a discount of 1 0% to their prices. Display the discounted prices along with other book details.
