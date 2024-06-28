96ed7b053e9d0a13759668382a78a126184034dd
niharika.shetty26 <niharika.shetty@successive.tech>
2024-06-28 16:49:45 +0530

    fix:

--- Day-48/Day-48.py
+++ Day-48/Day-48.py
@@ -8,12 +8,8 @@ from connection import execute_and_print_query, update_table
 def main():
     print("Books table before update:")
     execute_and_print_query("SELECT * FROM Books;")
-
-    # Test Case 1: Update books where price>=20 update it with 30
     update_table("Books", {"price": 30}, "price>=20", transactional=True)
     print("Test Case 1: Updated books where price>=20")
-
-    # Test Case 2: Update books where genre is 'Classic' and price is greater than 15
     update_table("Books", {"price": 25}, "genre = 'Classic'", "price > 15", transactional=True)
     print("Test Case 2: Update books where genre is 'Classic' and price > 15 completed.")
 
