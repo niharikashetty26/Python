217f5ccd34bcb9959b43285c02f30461aea66624
niharika.shetty26 <niharika.shetty@successive.tech>
2024-08-14 14:31:00 +0530

    fix:14-Aug

--- Day-78/school_api/api/serializers.py
+++ Day-78/school_api/api/serializers.py
@@ -1,13 +1,54 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Course
 
-from rest_framework import serializers
-from .models import Student
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['id', 'name', 'description']
+
 
 class StudentSerializer(serializers.ModelSerializer):
+    courses = CourseSerializer(many=True)
+
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = ['id', 'first_name', 'last_name', 'email', 'date_of_birth', 'enrollment_date', 'courses']
 
+    def create(self, validated_data):
+        courses_data = validated_data.pop('courses')
+        student = Student.objects.create(**validated_data)
+        for course_data in courses_data:
+            course, created = Course.objects.get_or_create(**course_data)
+            student.courses.add(course)
+        return student
 
+    def update(self, instance, validated_data):
+        courses_data = validated_data.pop('courses')
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.date_of_birth = validated_data.get('date_of_birth', instance.date_of_birth)
+        instance.enrollment_date = validated_data.get('enrollment_date', instance.enrollment_date)
+        instance.save()
+
+        instance.courses.clear()
+        for course_data in courses_data:
+            course, created = Course.objects.get_or_create(**course_data)
+            instance.courses.add(course)
+
+        return instance
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields = ['username', 'password', 'email']
+    def create(self, validated_data):
+        user=User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
 
--- Day-78/school_api/api/urls.py
+++ Day-78/school_api/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import StudentListCreateView, StudentRetrieveUpdateDestroyView
+from .views import StudentListCreateView, StudentRetrieveUpdateDestroyView, StudentListRetrieveView
+from .views import RegisterView, CustomAuthToken, LogoutView
 
 urlpatterns = [
     path('students/', StudentListCreateView.as_view(), name='student-list-create'),
     path('students/<int:pk>/', StudentRetrieveUpdateDestroyView.as_view(), name='student-detail'),
+    path('students-list-retrieve/', StudentListRetrieveView.as_view(), name='student-list-retrieve'),
+    path('students-list-retrieve/<int:pk>/', StudentListRetrieveView.as_view(), name='student-list-retrieve-specific'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='login'),
+
 ]
